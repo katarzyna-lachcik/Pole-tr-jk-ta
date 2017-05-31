330c2c7fcfdb2a32a1112c093b7b80683df3d5b6
Katarzyna Lachcik <kaisek@poczta.fm>
2017-05-31 18:49:06 +0100

    my first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//scripts.js
+var a = prompt('base a='),
+	h = prompt('height h='),
+	triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
